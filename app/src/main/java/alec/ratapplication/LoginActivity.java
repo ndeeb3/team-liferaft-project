2c7708e749c610eedaf2658d3a34a2a88b94a4a0
Alec Gretta <agretta3@gatech.edu>
2017-09-25 16:19:11 -0400

    Welcome Screen and parts of login applied

--- app/src/main/java/alec/ratapplication/LoginActivity.java
+++ app/src/main/java/alec/ratapplication/LoginActivity.java
@@ -3,6 +3,7 @@ package alec.ratapplication;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -87,6 +88,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 attemptLogin();
+            }
+        });
+
+        Button cancelLogin = (Button) findViewById(R.id.cancelLoginButton);
+        cancelLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(LoginActivity.this, Logout.class);
             }
         });
 
