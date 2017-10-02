ff66af623947c4d6475f92589a4d1a7c6f8c585c
Sonia Thakur <sthakur26@gmail.com>
2017-10-02 16:25:10 -0400

    removed enum from RatSightingReport

--- app/src/main/java/alec/ratapplication/User.java
+++ app/src/main/java/alec/ratapplication/User.java
@@ -1,9 +1,12 @@
 package alec.ratapplication;
+
+import java.io.Serializable;
+
 /**
  * Created by Sonia Thakur on 10/2/2017.
  */
 
-public class User {
+public class User implements Serializable {
     /*login name (in recognition of current popular trends, this can be the email address)
     password
     account state (locked or unlocked)
@@ -19,28 +22,52 @@ public class User {
         this.password = password;
         this.contactInfo = contactInfo;
     }
-
+    /**
+     * @return the login name for the user
+     */
     public String getLoginName() {
         return loginName;
     }
+    /**
+     *
+     * @param loginName the login name of the user
+     */
     public void setLoginName(String loginName) {
         this.loginName = loginName;
     }
+    /**
+     * @return the password of the user
+     */
     public String getPassword() {
         return password;
     }
+    /**
+     * @param password new password for the user
+     */
     public void setPassword(String password) {
         this.password = password;
     }
+    /**
+     * @return whether or not the account is locked
+     */
     public boolean getAccountStatus() {
         return locked;
     }
-    public void setAccountSTate(boolean accountState) {
+    /**
+     * @param accountState the new account state of the user
+     */
+    public void setAccountState(boolean accountState) {
         locked = accountState;
     }
+    /**
+     * @return the contact information of the user
+     */
     public String getContactInfo() {
         return contactInfo;
     }
+    /**
+     * @param contactInfo the new contact information of the user
+     */
     public void setContactInfo(String contactInfo) {
         this.contactInfo = contactInfo;
     }
